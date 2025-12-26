16931c6b6c9ceda5a546172151a460aa6cef8afb
chakunde2004 <om.chakunde@mitaoe.ac.in>
2025-12-26 19:12:33 +0530

    Add backend and frontend source code

---
+++ job-backend/app.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const authRoutes = require("./routes/auth");
+const adminRoutes = require("./routes/admin");
+const providerRoutes = require("./routes/provider");
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+// Load MONGODB_URI from .env file
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+// Uncomment if static file serving is needed
+// app.use("/resumes", express.static(path.join(__dirname, "resumes")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
+
+app.use("/auth", authRoutes);
+app.use("/admin", adminRoutes);
+app.use("/provider", providerRoutes);
+app.use("/user", userRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+
+  res.status(status).json({
+    message: message,
+    data: data,
+  });
+});
+
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to Database");
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection error:", err);
+  });
