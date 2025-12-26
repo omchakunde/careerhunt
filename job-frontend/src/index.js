39aa384411b9b505c1bfa59e8275ad823376df26
chakunde2004 <om.chakunde@mitaoe.ac.in>
2025-12-26 19:24:26 +0530

    Fix CSS issues for Vercel deployment

--- job-frontend/src/index.js
+++ job-frontend/src/index.js
@@ -6,6 +6,10 @@ import App from "./App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./login/Reducers/rootReducer";
+import './index.css';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // if using bootstrap
+
 
 const store = createStore(rootReducer);
 
